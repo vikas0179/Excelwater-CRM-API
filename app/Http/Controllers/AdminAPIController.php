9ef4071fba92d43288752554027b3eb56171b616
Rakesh Solanki <rakeshsolanki60@gmail.com>
2025-07-14 11:14:25 +0530

    api changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -4523,12 +4523,10 @@ class AdminAPIController extends Controller
 			'mobile' => 'required|unique:users',
 			'email' => 'required|email|unique:users',
 			'billing_address' => 'required',
-			// 'billing_landmark' => 'required',
 			'billing_city' => 'required',
 			'billing_state' => 'required',
 			'billing_zipcode' => 'required',
 			'shipping_address' => 'required',
-			// 'shipping_landmark' => 'required',
 			'shipping_city' => 'required',
 			'shipping_state' => 'required',
 			'shipping_zipcode' => 'required',
@@ -4537,12 +4535,10 @@ class AdminAPIController extends Controller
 			'mobile.required' => 'Please Enter Mobile Number',
 			'email.required' => 'Please Enter Email ID',
 			'billing_address.required' => 'Please Enter Billing Address',
-			// 'billing_landmark.required' => 'Please Enter Billing Landmark',
 			'billing_city.required' => 'Please Enter Billing City',
 			'billing_state.required' => 'Please Enter Billing State',
 			'billing_zipcode.required' => 'Please Enter Billing Zip-Code',
 			'shipping_address.required' => 'Please Enter Shipping Address',
-			// 'shipping_landmark.required' => 'Please Enter Shipping Landmark',
 			'shipping_city.required' => 'Please Enter Shipping City',
 			'shipping_state.required' => 'Please Enter Shipping State',
 			'shipping_zipcode.required' => 'Please Enter Shipping Zip-Code',
@@ -4744,9 +4740,9 @@ class AdminAPIController extends Controller
 		}
 
 		if (!empty($NewAry)) {
-			return $this->response("", false, $NewAry);
+			return $this->response(NULL, false, $NewAry);
 		} else {
-			return $this->response("Not Found Product List.", true);
+			return $this->response(NULL, true);
 		}
 	}
 
@@ -4966,6 +4962,77 @@ class AdminAPIController extends Controller
 			return $this->response("", false, $ProductStock);
 		} else {
 			return $this->response("Product Stock Not Found", true);
+		}
+	}
+
+
+
+
+
+	// App
+	public function DraftInvoice()
+	{
+		$ProductMasterList = Invoice::leftJoin('users', 'users.id', '=', 'invoice.customer_id')
+			// ->where('invoice.transaction_type', 1)
+			->select('invoice.id', 'invoice.invoice_no', 'users.name as customer_name')
+			->get();
+
+		if (!empty($ProductMasterList) && count($ProductMasterList) > 0) {
+			return $this->response("", false, $ProductMasterList);
+		} else {
+			return $this->response("Invoice Not Found", true);
+		}
+	}
+
+	public function SaveInvoice(Request $request)
+	{
+		$validator = Validator::make($request->all(), [
+			'invoice_id' => 'required',
+			'product_code' => 'required|array',
+		], [
+			'invoice_id.required' => 'Please Enter Invoice ID',
+			'product_code.required' => 'Please Enter Product Code',
+		]);
+
+		if ($validator->fails()) {
+			return $this->response($validator->errors()->first(), true);
+		}
+
+		$product_codes = $request->product_code;
+		$invoice_id = $request->invoice_id;
+
+		$ProductCodeExit = [];
+		if (!empty($product_codes)) {
+			foreach ($product_codes as $product_code) {
+				$GetProductStockID = ProductStock::where('product_code', $product_code)->select('id')->first();
+				if (!empty($GetProductStockID->id)) {
+					if (isset($ProductCodeExit[$GetProductStockID->id])) {
+						$ProductCodeExit[$GetProductStockID->id] += 1;
+					} else {
+						$ProductCodeExit[$GetProductStockID->id] = 1;
+					}
+				}
+			}
+
+			$InvoiceItemList = InvoiceItem::where('invoice_id', $invoice_id)->select('id', 'qty', 'product_stock_id')->get();
+			if (!empty($InvoiceItemList) && count($InvoiceItemList) > 0) {
+				foreach ($InvoiceItemList as $key => $item) {
+					$product_stock_ids = explode(',', $item['product_stock_id']);
+					$total_qty = 0;
+					foreach ($product_stock_ids as $pid) {
+						$pid = (int)$pid;
+						if (isset($ProductCodeExit[$pid])) {
+							$total_qty += $ProductCodeExit[$pid];
+						}
+					}
+					$item->qty = ($item->qty + $total_qty);
+					$item->save();
+				}
+			}
+
+			return $this->response("Successfully Changed!", false);
+		} else {
+			return $this->response("Error!", true);
 		}
 	}
 }
