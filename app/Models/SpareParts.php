2c66dadedde21d2b57b85f46a867f1d88a71695a
Rakesh Solanki <rakeshsolanki60@gmail.com>
2025-10-18 12:22:57 +0530

    Update SpareParts.php

--- app/Models/SpareParts.php
+++ app/Models/SpareParts.php
@@ -6,20 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class SpareParts extends Model
 {
-	protected $table = 'spare_parts';   
+        protected $table = 'spare_parts';
 
-	protected $fillable = [
-        'part_name',
-        'part_number',
-        'price',
-        'min_alert_qty',
-        'desc',
-        'image',
-	];
-	
-	protected $casts = [
-        'created_at'  => 'date:M d, Y h:i A',
-        'updated_at' => 'datetime:M d, Y h:i A',
-    ];
+        protected $fillable = [
+                'part_name',
+                'part_number',
+                'price',
+                'min_alert_qty',
+                'opening_stock',
+                'desc',
+                'image',
+                'stock_qty',
+        ];
 
+        protected $casts = [
+                'created_at'  => 'date:M d, Y h:i A',
+                'updated_at' => 'datetime:M d, Y h:i A',
+        ];
 }
